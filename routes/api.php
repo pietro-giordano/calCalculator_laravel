e80490e4913c46952a495ae7e737512844f0d847
Pietro Giordano <giopi87@gmail.com>
2023-11-12 17:27:13 +0100

    aggiunta rotte api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\IngredientController;
+use App\Http\Controllers\RecipeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +17,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+      return $request->user();
+});
+
+Route::name('api.')->group(function () {
+      Route::resource('ingredients', IngredientController::class);
+      Route::resource('recipes', RecipeController::class);
 });
